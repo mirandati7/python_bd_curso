1d33c79967cb70978bf20c22a7c36f0a2de3c5cc
Alex Miranda <you@example.com>
2025-05-29 21:59:27 -0300

    Projeto BD

---
+++ cliente.py
@@ -0,0 +1,69 @@
+from conexao import conecta_db
+
+def menu_cliente():
+    print ("|-------------------------------------------|")
+    print ("|      Cadastro de Cliente                  |")
+    print ("|-------------------------------------------|")
+    print("|        1  -  Listar Clientes               |")
+    print("|        2  -  Consultar um Cliente por(ID)  |")
+    print("|        3  -  Inserir                       |")
+    print("|        4  -  Alterar                       |")
+    print("|        5  -  Deletar                       |")
+    print("|        6  -  Sair                          |")
+    print ("|-------------------------------------------|")
+
+    while True: 
+        opcao = input("Escolha uma opção:  ")
+        conexao = conecta_db()
+
+        if opcao == "1":
+            listar_clientes(conexao)
+        elif opcao == "2":
+            listar_clientes(conexao)
+            consultar_cliente_por_id(conexao)
+        elif opcao == "3":
+            inserir_cliente(conexao)
+            listar_clientes(conexao)
+        elif opcao == "4":
+            print("Alterar")
+        elif opcao == "5":
+            print("Deletar")
+        elif opcao == "6":
+            print("Sair")
+            break
+        else:
+            print("Opção invalida, tente novamente")
+
+
+def listar_clientes(conexao):
+    cursor = conexao.cursor()
+    # Execução do select no banco de dados
+    cursor.execute("select id,nome from cliente order by id asc")
+    # recuperar todos registros
+    registros = cursor.fetchall()
+    print("|----------------------------------------|")
+    for registro in registros:
+        print(f"| ID: {registro[0]}  - Nome: {registro[1]} ")
+    print("|----------------------------------------|")
+
+
+def consultar_cliente_por_id(conexao):
+    id = input("Digite o ID: ")
+    cursor = conexao.cursor()
+    cursor.execute("select id,nome from cliente where id = " + id)
+    registro = cursor.fetchone()
+
+    if registro is None:
+        print("Cliente não encontrado:")
+    else:
+        print(f"| ID ..: {registro[0]} ")
+        print(f"| Nome : {registro[1]} ")
+
+
+def inserir_cliente(conexao):
+    print("Inserindo o Cliente ..: ")
+    cursor = conexao.cursor()
+    nome = input("Nome :")
+    sql_insert = "insert into cliente (nome) values ('" + nome + "')"
+    cursor.execute(sql_insert)
+    conexao.commit()
